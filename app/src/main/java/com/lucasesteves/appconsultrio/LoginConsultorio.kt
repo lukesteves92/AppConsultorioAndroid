7392a45ad484f8521b98c1d3c6d844a5b22aca6c
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-07-01 02:24:45 -0300

    Login Firebase

--- app/src/main/java/com/lucasesteves/appconsultrio/LoginConsultorio.kt
+++ app/src/main/java/com/lucasesteves/appconsultrio/LoginConsultorio.kt
@@ -1,15 +1,15 @@
 package com.lucasesteves.appconsultrio
-
+import android.util.Log
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.google.firebase.installations.remote.FirebaseInstallationServiceClient
+import android.app.Activity
 import com.lucasesteves.appconsultrio.databinding.ActivityLoginConsultorioBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+
 
 
 class LoginConsultorio : AppCompatActivity() {
@@ -17,8 +17,12 @@ class LoginConsultorio : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityLoginConsultorioBinding
 
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         binding = ActivityLoginConsultorioBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -27,7 +31,10 @@ class LoginConsultorio : AppCompatActivity() {
 
 
 
+
         }
     }
+
+
 
 
